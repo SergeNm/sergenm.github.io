7183f79880ef3a31ab4585ac3389a1610d789bc4
unknown <nmugishaserge@gmail.com>
2022-02-08 11:24:14 +0200

    update home (minor)

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,6 +1,6 @@
 class MainController {
     home(req, res) {
-        return res.send("You can find the documentation of this api here: https://sergenm-brand.herokuapp.com/docs/");
+        return res.send("You can find the documentation of this api here: <a href=\"https://sergenm-brand.herokuapp.com/docs/\"> here </a>");
     }
 }
 export default MainController;
